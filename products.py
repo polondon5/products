26ef2e9c10bc55c2b07dbc14db65904457b2594a
Po Chiang <polondon5@gmail.com>
2020-03-14 10:48:26 +0800

    add encoding utf-8

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q': #離開
 		break
 	price = input('請輸入商品價格：')
+	price = int(price)
 	p = [] #p = [name, price]
 	p.append(name)
 	p.append(price)
@@ -12,3 +13,12 @@ print(products)
 
 for p in products
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') #\n 分行、換行
+
+#CSV是電腦最常使用的資料儲存模式，而且可以使用excel打開，非常方便。
+#資料通常都是用逗點做區隔，才能有資料儲存區隔，不然會塞在同一格，很難使用。
+#每一單筆資料一定要換行，加入換行符號
